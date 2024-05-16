a8964d4180cf2e29b042acb3138900cdaf8df2a8
MT XA <21020382@vnu.edu.vn>
2024-05-16 23:58:56 +0700

    favourite screen

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -17,69 +17,98 @@ class FavoriteScreen extends StatefulWidget {
 }
 
 class _FavoriteScreenState extends State<FavoriteScreen> {
-  List<PlaceSummaryModel>? favoritePlaces;
+  List<PlaceSummaryModel> favoritePlaces = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _fetchGetFavoritePlaces();
+  }
+
+  Future<void> _fetchGetFavoritePlaces() async {
+    try {
+      final response = await getFavoritePlaces();
+      if (response.statusCode == 200) {
+        List<dynamic> jsonData = jsonDecode(response.body);
+        setState(() {
+          favoritePlaces = jsonData.map((item) => PlaceSummaryModel.fromJson(item)).toList();
+        });
+      } else {
+        // Handle error response
+      }
+    } catch (error) {
+      // Handle error
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text("Địa điểm yêu thích",style: Theme.of(context).textTheme.headlineMedium,)),
-      ),
-      body: Container(
-        padding: EdgeInsets.only(top: 16.0, left: 8.0, right: 8.0, bottom: 16.0),
-        child: Column(
+        appBar: AppBar(
+          title: Center(child: Text("Địa điểm yêu thích",style: Theme.of(context).textTheme.headlineMedium,)),
+        ),
+        body: Container(
+          padding: EdgeInsets.only(top: 16.0, left: 8.0, right: 8.0, bottom: 16.0),
+          child: favoritePlaces.isEmpty ?? true
+              ? Center(
+              child: Text(
+                "Không có địa điểm yêu thích",
+                style: TextStyle(fontSize: 30),
+              )
+          )
+              : Column(
             children: [
-               Expanded(
-                 child: SizedBox(
-                   width: double.infinity,
-                   height: 500,
-                   child: ScrollConfiguration(
-                       behavior: const ScrollBehavior(),
-                       child: GridView(
-                         scrollDirection: Axis.vertical,
-                         gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
-                           crossAxisCount: 2,
-                           // mainAxisExtent: 280,
-                           crossAxisSpacing: 10,
-                           mainAxisSpacing: 10,
-                           childAspectRatio: 0.5 // cái này sửa lại cho đúng tỉ lệ của máy pixel 3A API 34
-                         ),
-                         children: favoritePlaces!.map(
-                               (place) => PlaceItemWidget(
-                                   place: place,
-                                   onPressed: () {
-                                     print(place.placeId);
-                                     Navigator.push(
-                                       context,
-                                       PageRouteBuilder(
-                                         transitionDuration: Duration(milliseconds: 500),
-                                         pageBuilder: (context, animation, secondaryAnimation) {
-                                           return PlaceDetailScreen(placeId: place.placeId);
-                                         },
-                                         transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                                           const begin = Offset(1.0, 0.0);
-                                           const end = Offset.zero;
-                                           const curve = Curves.ease;
+              Expanded(
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 500,
+                  child: ScrollConfiguration(
+                      behavior: const ScrollBehavior(),
+                      child: GridView(
+                        scrollDirection: Axis.vertical,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            // mainAxisExtent: 280,
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
+                            childAspectRatio: 0.5 // cái này sửa lại cho đúng tỉ lệ của máy pixel 3A API 34
+                        ),
+                        children: favoritePlaces.map(
+                                (place) => PlaceItemWidget(
+                                place: place,
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    PageRouteBuilder(
+                                      transitionDuration: Duration(milliseconds: 500),
+                                      pageBuilder: (context, animation, secondaryAnimation) {
+                                        return PlaceDetailScreen(placeId: place.placeId);
+                                      },
+                                      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                                        const begin = Offset(1.0, 0.0);
+                                        const end = Offset.zero;
+                                        const curve = Curves.ease;
 
-                                           var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                                        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
-                                           return SlideTransition(
-                                             position: animation.drive(tween),
-                                             child: child,
-                                           );
-                                         },
-                                       ),
-                                     );
-                                   }
-                               )
-                         ).toList(),
-                       )
-                   ),
-                 ),
-               )
+                                        return SlideTransition(
+                                          position: animation.drive(tween),
+                                          child: child,
+                                        );
+                                      },
+                                    ),
+                                  );
+                                }
+                            )
+                        ).toList(),
+                      )
+                  ),
+                ),
+              )
             ],
           ),
-      )
+        )
     );
   }
 }
