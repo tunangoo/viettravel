c6352bebb4bceee4d1999fc3aa1a53f764db8c6e
MT XA <21020382@vnu.edu.vn>
2024-05-16 23:54:33 +0700

    update home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,35 +1,78 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:viettravel/widgets/recommended_places.dart';
+import '../models/user_model.dart';
+import 'package:viettravel/services/api_handle.dart';
 
-class HomeScreen extends StatelessWidget {
-
+class HomeScreen extends StatefulWidget {
   const HomeScreen({
-    Key? key
+    Key? key,
   }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var user;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchGetUserInfo();
+  }
+
+  Future<void> _fetchGetUserInfo() async {
+    try {
+      final response = await getUserInfo();
+      if (response.statusCode == 200) {
+        setState(() {
+          user = UserModel.fromJson(response.body);
+        });
+      } else {
+        // Handle error response
+      }
+    } catch (error) {
+      // Handle error
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: 100,
         elevation: 0,
         backgroundColor: Colors.transparent,
         foregroundColor: Colors.black,
-        title: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+        title: Container(
+          padding: EdgeInsets.only(top: 15, bottom: 10),
+          child: Row(
             children: [
-              const Text("Xin chào"),
-              // Text(
-              //   userFullName, // Hiển thị tên người dùng từ đối tượng user
-              //   "tên người dùng",
-              //   style: Theme.of(context).textTheme.labelMedium,
-              // )
-            ]
+              Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(23), color: Colors.grey[300]),
+                child: Row(
+                  children: [
+                    ClipOval(
+                      child: Image.asset("assets/images/profile.png",
+                      height: 40,
+                      width: 40,),
+                    ),
+                    SizedBox(width: 10,),
+                    Center(child: Text("Bui Minh Quan", style: TextStyle(fontSize: 17),))
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
         actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.notifications_outlined),
+          Center(
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.notifications_outlined, size: 30,),
+            ),
           ),
           const Padding(padding: EdgeInsets.all(8.0))
         ],
@@ -37,16 +80,18 @@ class HomeScreen extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(14),
         children: [
-          const SizedBox(height: 20),
+          SizedBox(height: 20),
           Row(
             children: [
               Text(
-                "Khám phá khắp \n Việt Nam !",
-                style: Theme.of(context).textTheme.headlineLarge,
+                "Khám phá khắp \nViệt Nam !",
+                style: TextStyle(
+                  fontSize: 40,
+                ),
               ),
             ],
           ),
-          const SizedBox(height: 85),
+          const SizedBox(height: 40),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -68,7 +113,7 @@ class HomeScreen extends StatelessWidget {
               )
             ],
           ),
-          const SizedBox(height: 20),
+          const SizedBox(height: 10),
           const RecommendedPlaces(),
         ],
       )
